6a2700466f256e83202cf6b2b87bcefa8efd7f04
BluePhoenixRa <r.arefin@gmail.com>
2015-08-14 15:31:15 -0400

    Categories Edit and Destroy fucntionality added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,15 +15,32 @@ class CategoriesController < ApplicationController
   end
 
   def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category updated successfully!"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
+    @page_title = 'Edit Category'
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    if @category.destroy
+      flash[:alert] = "Category deleted successfully!"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
   end
 
   def index
+    @page_title = 'Categories'
     @categories = Category.all
   end
 
