3de73c5dd719e0e4516548e5d56c999d6584d3a5
BluePhoenixRa <r.arefin@gmail.com>
2015-08-14 16:29:12 -0400

    Added Publisher Edit and Destroy controllers and pages

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -7,7 +7,7 @@ class PublishersController < ApplicationController
   def create
     @publisher = Publisher.new(publisher_params)
     if @publisher.save
-      flash[:notice] = "New Publisher added successfully"
+      flash[:notice] = "New Publisher added successfully!"
       redirect_to publishers_path
     else
       render 'new'
@@ -15,15 +15,33 @@ class PublishersController < ApplicationController
   end
 
   def update
+    @publisher = Publisher.find(params[:id])
+    if @publisher.update(publisher_params)
+      flash[:notice] = "Publisher edited successfully!"
+      redirect_to publishers_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @publisher = Publisher.find(params[:id])
+    @page_title = 'Edit Publisher: '+@publisher.name
   end
 
   def destroy
+    @publisher = Publisher.find(params[:id])
+    if @publisher.destroy
+      flash[:alert] = "Publisher deleted successfully!"
+      redirect_to publishers_path
+    else
+      render 'new'
+    end
   end
 
   def index
+    @page_title = 'Publishers'
+    @publisher = Publisher.all
   end
 
   def show
